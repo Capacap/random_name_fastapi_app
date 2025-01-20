d1bd6f0dee1caf552e28ef25f9c7d28608f8d75a
Capacap <simon.sorkin@outlook.com>
2025-01-20 12:53:28 +0100

    added logging

--- app/main.py
+++ app/main.py
@@ -1,10 +1,46 @@
+import time
 import uvicorn
+import logging
 import app.namegen as ng
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Response
 from typing import Dict, List
 
 app = FastAPI()
 df = ng.process_csv_data("./data/firstnames.csv")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("app.log"),
+        logging.StreamHandler()
+    ]
+)
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next) -> Response:
+    """
+    Middleware to log incoming HTTP requests.
+
+    Args:
+        request (Request): The incoming HTTP request.
+        call_next (Callable): The next middleware or request handler.
+
+    Returns:
+        Response: The HTTP response from the next middleware or request handler.
+    """
+    idem = time.strftime("%Y%m%d-%H%M%S")
+    logging.info(f"request id={idem} path={request.url.path}")
+    start_time = time.time()
+
+    response = await call_next(request)
+
+    process_time = time.time() - start_time
+    formatted_process_time = '{0:.2f}'.format(process_time * 1000)
+    logging.info(f"request id={idem} completed_in={formatted_process_time}ms status_code={response.status_code}")
+
+    return response
+
 
 @app.get("/status", response_model=Dict)
 def get_status() -> Dict:
@@ -87,7 +123,7 @@ def get_random_female_names(country: str, count: int) -> Dict[str, List[str]]:
         raise HTTPException(status_code=400, detail="Invalid country name. Please provide a valid country name.")
     if count <= 0:
         raise HTTPException(status_code=400, detail="Count must be a positive integer.")
-    if count > df[df[country] != 0].shape[0]:
+    if count > df[df[country] != 0].shape[0]]:
         raise HTTPException(status_code=400, detail="Count exceeds the number of available names. Please provide a smaller count.")
 
     random_female_names = ng.get_random_female_names(df, country, count)
